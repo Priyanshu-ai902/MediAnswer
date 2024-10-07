c8bc369cb614e390b2f53a13deda1b06d2045b69
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-10-07 18:27:58 +0530

    shadcn-ui added with dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
+import { ModeToggle } from '@/components/modetoggle'
 import React from 'react'
 
 type Props = {}
 
 const HomeComponent = (props: Props) => {
-  return(
-    <div>
-hellp
+  return (
+    <div className='grid h-screen w-full'>
+      <div className="flex flex-col">
+        <header className='sticky top-0 z-10 h-[57px] bg-background flex items-center gap-1 border-b px-4'>
+          <h1 className='text-xl font-semibold'>
+            <span className='text-[#5e9dfb]'>Medi</span>
+            <span className='text-[#255dd5]'>Answer</span>
+          </h1>
+
+          <div className="w-full flex flex-row justify-end gap-2">
+            <ModeToggle />
+          </div>
+        </header>
+      </div>
     </div>
   )
 }
