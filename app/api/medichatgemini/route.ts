e1f38b5dd850f1ec650e7063f01bdcce1425a6d6
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-10-12 18:42:27 +0530

    pinecone initialization

--- app/api/medichatgemini/route.ts
+++ app/api/medichatgemini/route.ts
@@ -1,6 +1,20 @@
+import { Message } from "ai/react";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { queryPineconeVectorStore } from "@/utils";
+
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY! })
+
 export async function POST(req: Request, res: Response) {
     const reqBody = await req.json()
     console.log(reqBody)
+    const messages: Message[] = reqBody.messages
+    const userQuestion = messages[messages.length - 1].content
+    const reportData = reqBody.data.reportData
+
+    const serachQuery = `Patient medical report says: \n${reportData} \n\n ${userQuestion}`
+    
+    const retrievals = await queryPineconeVectorStore(pc, 'medianswer', 'ns1', serachQuery)
 
     return new Response("dummy response", { status: 200 })
+
 }
---
+++ utils.ts
@@ -0,0 +1,12 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+
+export async function queryPineconeVectorStore(
+    client: Pinecone,
+    indexName: string,
+    namespace: string,
+    serachQuery: string
+): Promise<string> {
+    
+    return ""
+}
+
